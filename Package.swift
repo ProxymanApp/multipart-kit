48d41c7701bd1d8487d453db6512ad85415762ca
Nghia Tran <vinhnghiatran@gmail.com>
2021-09-23 20:38:57 +0700

    Bump Min macOS 10.13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "multipart-kit",
     platforms: [
-       .macOS(.v10_12)
+       .macOS(.v10_13)
     ],
     products: [
         .library(name: "MultipartKit", targets: ["MultipartKit"]),
